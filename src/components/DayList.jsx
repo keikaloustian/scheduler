c7d728cd8e53dda4b540e574fab1df6f31c7db95
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-01 18:36:46 -0700

    Implement and Storybook test DayList component

---
+++ src/components/DayList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import DayListItem from './DayListItem';
+
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+
+export default function DayList(props) {
+
+  const daysParsed = props.days.map((dayObj) => {
+    return <DayListItem
+      key={dayObj.id}
+      name={dayObj.name}
+      spots={dayObj.spots}
+      selected={props.selected === dayObj.name}
+      setDay={props.setDay}
+    />
+  });
+
+  return (
+    <ul>
+      {daysParsed}
+    </ul>
+  );
+}
