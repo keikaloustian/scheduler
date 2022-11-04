cf935afbca907505896b56e6490f98cd5ae9e876
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-03 20:50:59 -0700

    Write getAppointmentsForDay selector function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+  
+  if (!state.days.length) {
+    return [];
+  }
+
+  let appointmentIDs = [];
+  for (let eachDay of state.days) {
+    if (eachDay.name === day) {
+      appointmentIDs = eachDay.appointments;
+    }
+  }
+
+  const appointmentsArray = Object.values(state.appointments);
+
+  let output = appointmentsArray.filter((appointmentObj) => {
+    return appointmentIDs.includes(appointmentObj.id)
+  })
+
+  return output;
+}
