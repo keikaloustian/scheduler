59e470da3db7a4849b65af825f6dcc4a49020ece
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-05 12:45:35 -0700

    Build and test custom hook

---
+++ src/hooks/useVisualMode.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    if (replace) {
+      const replacedHistory = [...history];
+      replacedHistory.pop();
+      setHistory([...replacedHistory, newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+      setMode(newHistory[newHistory.length - 1]);
+      setHistory(newHistory);
+    }
+  };
+
+  return { mode, transition, back };
+}
