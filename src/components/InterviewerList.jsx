d23c4673a046a9131fa6ad0cc1d0cca171bfb994
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-08 09:38:21 -0800

    Install prop-types and implement check on interviewers array prop to InterviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import 'components/InterviewerList.scss'
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   
   const interviewersArray = props.interviewers.map((interviewerObj) => {
     return (
@@ -26,3 +27,11 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+
+
+export default InterviewerList;
