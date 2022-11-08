6d9ac588d59f356ab385fb7e62b8f5ffcfdc3298
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-08 09:38:41 -0800

    Refactor minor

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,25 +2,6 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-
 export default function DayList(props) {
 
   const daysArray = props.days.map((dayObj) => {
